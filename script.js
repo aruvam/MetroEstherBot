c6c94a6fbf5406fb1dfcaa0263a5e455ad144fa2
aruvam <aruvam@users.noreply.github.com>
2016-05-04 16:20:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const scriptRules = require('./script.json');
 
 module.exports = new Script({
     processing: {
-        prompt: (bot) => bot.say('Beep boop...'),
+        //prompt: (bot) => bot.say('Beep boop...'),
         receive: () => 'processing'
     },
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you want to learn about Metropolis? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
